923eb8212d7309f23a5a9904e150f3f15158b722
Egor <egormikulic@gmail.com>
2020-12-16 18:37:17 +0300

    Add components

--- stardb/src/index.js
+++ stardb/src/index.js
@@ -14,11 +14,43 @@ class SwapiService {
     const res = await this.getResource(`/people/`);
     return res.results;
    }
+
    getPerson(id){
     return this.getResource(`/people/${id}`)
   }
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+   }
+   getPlanet(id){
+    return this.getResource(`/planets/${id}`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+   }
+
+   getStarShip(id){
+    return this.getResource(`/starships/${id}`)
+  }
 }
 
+
+ 
+
 const swapi = new SwapiService();
-swapi.getAllPeople().then((body) =>
-console.log(body));
+swapi.getAllPeople().then((people) =>
+people.forEach((p) => {
+  console.log(p.name)
+}));
+
+swapi.getPerson(6).then((p) =>
+{
+  console.log(p.name)}
+);
+
+swapi.getAllPlanets().then((people) =>
+people.forEach((p) => {
+  console.log(p.name)
+}));
